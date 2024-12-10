14239d1884f83f0e4d499c2bddebc89dcb266848
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2024-12-09 22:29:12 -0600

    Add basic NET Standard Libray implemented in Console App

---
+++ MiAppStandard/Program.cs
@@ -0,0 +1,13 @@
+﻿using MyStandardLibrary;
+using System;
+
+class Program {
+    static void Main(){
+        //Write once, run anywhere
+        //A library that targets .NET Standard can be used on all 
+        //Code Sharing, Platform Independence and API consistency
+        Console.WriteLine($"Subtraction: { MyMathematics.Subtraction(5, 3) }");
+        Console.WriteLine($"Addition: {MyMathematics.Addition(5, 3)}");
+
+    }
+}
---
+++ MyStandardLibrary/MyMathematics.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyStandardLibrary
+{
+    public class MyMathematics
+    {
+        public static int Addition (int a, int b) => a + b;
+        public static int Subtraction (int a, int b) => a - b;
+
+    }
+}
