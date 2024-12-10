e5581c478424ad61e1022d0f797f9382bf985bb9
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2024-12-10 11:34:36 -0600

    Add MathLibrary and FrameworkApp using etstandard1.0 and net4.0, error NU1201: Project MathLibrary is not compatible with net40 (.NETFramework,Version=v4.0). Project MathLibrary supports: netstandard1.0 (.NETStandard,Version=v1.0)

---
+++ FrameworkApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using MathLibrary;
+using System;
+
+namespace FrameworkApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //error NU1201: Project MathLibrary is not compatible with net40 (.NETFramework,Version=v4.0). Project MathLibrary supports: netstandard1.0 (.NETStandard,Version=v1.0)
+             Console.WriteLine("Calculating the derivative of the function f(x) = x^2 at x = 3");
+             // Function f(x) = x^2
+             Fun<double, double> function = x => x * x;
+             // Calculate the derivate at x = 3
+             double derivate = NumericalDifferentiation.Differentiate(function, 3);
+             Console.WriteLine($"The derivative of f(x) = x^2 at x = 3 is: {derivative}");
+        }
+    }
+}
---
+++ MathLibrary/NumericalDifferentiation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MathLibrary
+{
+    public static class NumericalDifferentiation
+    {
+        public static double Differentiate(System.Func<double, double> function, double x, double h = 1e-5){
+            return (function(x + h) - function(x - h)) / (2 * h);
+        }
+
+
+    }
+}
